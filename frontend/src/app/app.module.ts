4eadfc51a41474f73b1db3806fe5552239f689cf
Mateo Solano <115440962+MateoSolano65@users.noreply.github.com>
2025-04-20 18:19:17 -0500

    ✨ (app, module): refactor Material imports into a dedicated MaterialModule for better organization and maintainability

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,24 +12,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { ToyFormComponent } from './components/toy-form/toy-form.component';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
-
-// Material Modules
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatSelectModule } from '@angular/material/select';
+import { MaterialModule } from './shared/material.module'; // Importa el módulo de Material aquí
 
 @NgModule({
   declarations: [
@@ -44,32 +27,17 @@ import { MatSelectModule } from '@angular/material/select';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatDividerModule,
-    MatCardModule,
-    MatProgressSpinnerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatListModule,
-    MatSnackBarModule,
-    MatDialogModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    MatTableModule,
-    MatSliderModule,
-    MatSelectModule,
+    MaterialModule,
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
   bootstrap: [AppComponent],
 })
